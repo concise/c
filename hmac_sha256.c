4995c936326f00e09b8c99d904745fc9a4ab3f9d
- <->
2015-06-19 22:59:01 +0800

    Update hmac_sha256

--- hmac_sha256.c
+++ hmac_sha256.c
@@ -1,10 +1,17 @@
 #include "hmac_sha256.h"
 
+void hmac_sha256_starts(hmac_sha256_context *ctx, U32 klen, const U8 *key)
+{
+}
+
+void hmac_sha256_update(hmac_sha256_context *ctx, U32 ilen, const U8 *in)
+{
+}
+
+void hmac_sha256_finish(const hmac_sha256_context *ctx, U8 *out)
+{
+}
+
 void hmac_sha256(U32 klen, const U8 *key, U32 ilen, const U8 *in, U8 *out)
 {
-    (void) klen;
-    (void) key;
-    (void) ilen;
-    (void) in;
-    (void) out;
 }
