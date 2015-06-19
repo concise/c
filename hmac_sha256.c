4b03299469b56372ecd88cc7d1e6d7be3551605d
- <->
2015-06-19 21:07:31 +0800

    Will add hmac_sha256.c

---
+++ hmac_sha256.c
@@ -0,0 +1,10 @@
+#include "hmac_sha256.h"
+
+void hmac_sha256(U32 klen, const U8 *key, U32 ilen, const U8 *in, U8 *out)
+{
+    (void) klen;
+    (void) key;
+    (void) ilen;
+    (void) in;
+    (void) out;
+}
---
+++ hmac_sha256.h
@@ -0,0 +1,8 @@
+#ifndef __HMAC_SHA256_H__
+#define __HMAC_SHA256_H__
+
+#include "sha256.h"
+
+void hmac_sha256(U32, const U8 *, U32, const U8 *, U8 *);
+
+#endif
