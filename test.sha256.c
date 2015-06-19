09c3af95539320b80d5c6faed8c42d8a151bc5c2
- <->
2015-06-19 21:21:59 +0800

    Add a 1GB testing function for SHA-256

--- test.sha256.c
+++ test.sha256.c
@@ -1,6 +1,26 @@
 #include "sha256.h"
 #include <stdio.h>
 
+void super_sha256(void)
+{
+    /* The input length is 1 GiB */
+    U32 i;
+    U8 obuf[32];
+    sha256_context ctx;
+    U8 test_vector[64] = "abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmno";
+
+    sha256_init(&ctx);
+    for (i = 1; i <= 16777216; ++i) {
+        sha256_feed(&ctx, 64, test_vector);
+    }
+    sha256_done(&ctx, obuf);
+
+    for (i = 0; i < 32; ++i) {
+        printf("%02x", obuf[i]);
+    }
+    printf("\n");
+}
+
 void test_sha256(void)
 {
     int c;
@@ -14,7 +34,7 @@ void test_sha256(void)
     sha256_done(&ctx, obuf);
 
     for (c = 0; c < 32; ++c) {
-        printf("%02x", obuf[c] & 0xff);
+        printf("%02x", obuf[c]);
     }
     printf("\n");
 }
@@ -22,5 +42,6 @@ void test_sha256(void)
 int main(void)
 {
     test_sha256();
+    /* super_sha256(); */
     return 0;
 }
