2e05a09777d7a7712d1bb404e9e74fa3b013d57f
- <->
2015-06-20 01:13:30 +0800

    Update hmac_sha256_example.c

--- hmac_sha256_example.c
+++ hmac_sha256_example.c
@@ -5,23 +5,21 @@
 int main(void)
 {
     sha256_context sha256_ctx;
+    sha_info_t hmac_sha256_ctx;
     unsigned char bufferB[64];
     unsigned char bufferL[32];
-    sha_info_t sha_info;
     unsigned char obuf[32];
     int i;
 
-    sha_info.sha_context = &sha256_ctx;
-    sha_info.sha_starts  = &sha256_starts;
-    sha_info.sha_update  = &sha256_update;
-    sha_info.sha_finish  = &sha256_finish;
-    sha_info.bufferB     = bufferB;
-    sha_info.bufferL     = bufferL;
-    sha_info.B           = 64;
-    sha_info.L           = 32;
-
-
-    hmac(&sha_info, 0, 0, 0, 0, obuf);
+    hmac_sha256_ctx.sha_context = &sha256_ctx;
+    hmac_sha256_ctx.sha_starts  = &sha256_starts;
+    hmac_sha256_ctx.sha_update  = &sha256_update;
+    hmac_sha256_ctx.sha_finish  = &sha256_finish;
+    hmac_sha256_ctx.bufferB     = bufferB;
+    hmac_sha256_ctx.bufferL     = bufferL;
+    hmac_sha256_ctx.B           = 64;
+    hmac_sha256_ctx.L           = 32;
+    hmac(&hmac_sha256_ctx, 0, 0, 0, 0, obuf);
 
     for (i = 0; i < 32; ++i) {
         printf("%02x", obuf[i]);
