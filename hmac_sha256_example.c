a6ce11a12105b2115b1a75eff875d191a92b2b85
- <->
2015-06-20 01:32:06 +0800

    Refactor out the code into a hmac_sha256() function

--- hmac_sha256_example.c
+++ hmac_sha256_example.c
@@ -2,14 +2,14 @@
 #include "sha256.h"
 #include <stdio.h>
 
-int main(void)
+void hmac_sha256(int keylen, const unsigned char *key,
+                 int msglen, const unsigned char *msg,
+                 unsigned char *out)
 {
     sha256_context sha256_ctx;
     hmac_info_t hmac_sha256_ctx;
     unsigned char bufferB[64];
     unsigned char bufferL[32];
-    unsigned char obuf[32];
-    int i;
 
     hmac_sha256_ctx.sha_context = &sha256_ctx;
     hmac_sha256_ctx.sha_starts  = &sha256_starts;
@@ -19,7 +19,18 @@ int main(void)
     hmac_sha256_ctx.bufferL     = bufferL;
     hmac_sha256_ctx.B           = 64;
     hmac_sha256_ctx.L           = 32;
-    hmac(&hmac_sha256_ctx, 0, 0, 0, 0, obuf);
+
+    hmac(&hmac_sha256_ctx, keylen, key, msglen, msg, out);
+}
+
+int main(void)
+{
+    unsigned char obuf[32];
+    int i;
+
+
+    hmac_sha256(0, 0, 0, 0, obuf);
+
 
     for (i = 0; i < 32; ++i) {
         printf("%02x", obuf[i]);
