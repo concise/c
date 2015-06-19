8bc305972fc26d84e1d906b728690066acff6673
- <->
2015-06-19 21:33:25 +0800

    Update comments

--- sha256.h
+++ sha256.h
@@ -1,13 +1,13 @@
 #ifndef __SHA256_H__
 #define __SHA256_H__
 
-typedef unsigned char U8;
-typedef unsigned long int U32;
+typedef unsigned char U8;       /* Make sure it can hold 0 ~ 0xff           */
+typedef unsigned long int U32;  /* Make sure it can hold 0 ~ 0xffffffff     */
 typedef struct {
-    U32 runninghash[8];    /* The intermediate hash value (H0, ..., H7) */
-    U32 totalbitlen[2];    /* The bit length of the input message (l)   */
-    U8 msgchunk[64];       /* The last unprocessed message chunk        */
-    U8 msgchunklen;        /* The byte length of the unprocessed chunk  */
+    U32 runninghash[8];         /* The intermediate hash value (H0 ~ H7)    */
+    U32 totalbitlen[2];         /* The bit length (l) of the input message  */
+    U8 msgchunk[64];            /* The last unprocessed message chunk       */
+    U8 msgchunklen;             /* The byte length of the unprocessed chunk */
 } sha256_context;
 
 void sha256_init(sha256_context *);
