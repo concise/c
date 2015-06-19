04e4155cdd1cf93c433e50d7ec5312c4392de713
- <->
2015-06-19 17:49:58 +0800

    Remove #include

--- sha256.c
+++ sha256.c
@@ -1,4 +1,17 @@
-#include "sha256.h"
+typedef unsigned char U8;
+typedef unsigned long U32;
+
+typedef struct {
+    U32 runninghash[8];    /* The intermediate hash value (H0, ..., H7) */
+    U32 totalbitlen[2];    /* The bit length of the input message (l)   */
+    U8 msgchunk[64];       /* The last unprocessed message chunk        */
+    U8 msgchunklen;        /* The byte length of the unprocessed chunk  */
+} sha256_context;
+
+void sha256_init(sha256_context *);
+void sha256_feed(sha256_context *, U32, const U8 *);
+void sha256_done(const sha256_context *, U8 *);
+void sha256_hash(U32, const U8 *, U8 *);
 
 #define ROTL(a, b)      (((a) << (b)) | ((a) >> (32-(b))))
 #define ROTR(a, b)      (((a) >> (b)) | ((a) << (32-(b))))
