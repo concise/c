1c6d6fc4dbe3c0f448a028a93db890e7a6c1fd4d
Jong-Shian Wu <jongshian.wu@gmail.com>
2016-02-10 15:12:04 +0800

    -

--- sha256.c
+++ sha256.c
@@ -1,7 +1,7 @@
 #include "sha256.h"
 
-#define ROTR(a, b)      ((((a)&0xffffffff)>>(b))|(((a)<<(32-(b)))&0xffffffff))
-#define SHR(a, b)       (((a)&0xffffffff)>>(b))
+#define SHR(a, b)       (((a) & 0xffffffff) >> (b))
+#define ROTR(a, b)      (SHR(a, b) | (((a) << (32 - (b))) & 0xffffffff))
 #define Ch(x, y, z)     (((x) & (y)) ^ (~(x) & (z)))
 #define Maj(x, y, z)    (((x) & (y)) ^ ((x) & (z)) ^ ((y) & (z)))
 #define S0(x)           (ROTR(x,  2) ^ ROTR(x, 13) ^ ROTR(x, 22))
@@ -9,23 +9,24 @@
 #define Q0(x)           (ROTR(x,  7) ^ ROTR(x, 18) ^  SHR(x,  3))
 #define Q1(x)           (ROTR(x, 17) ^ ROTR(x, 19) ^  SHR(x, 10))
 
+static const uint32_t K[64] = {
+    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1,
+    0x923f82a4, 0xab1c5ed5, 0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
+    0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174, 0xe49b69c1, 0xefbe4786,
+    0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+    0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147,
+    0x06ca6351, 0x14292967, 0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
+    0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85, 0xa2bfe8a1, 0xa81a664b,
+    0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+    0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a,
+    0x5b9cca4f, 0x682e6ff3, 0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
+    0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+};
+
 static void process_one_block(sha256_context_t *ctx, const uint8_t *data)
 {
     int i;
     uint32_t a, b, c, d, e, f, g, h, t1, t2, m[64];
-    static const uint32_t K[64] = {
-        0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1,
-        0x923f82a4, 0xab1c5ed5, 0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
-        0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174, 0xe49b69c1, 0xefbe4786,
-        0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-        0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147,
-        0x06ca6351, 0x14292967, 0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
-        0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85, 0xa2bfe8a1, 0xa81a664b,
-        0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-        0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a,
-        0x5b9cca4f, 0x682e6ff3, 0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
-        0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
-    };
 
     for (i = 0; i <= 15; ++i) {
         m[i] = ((uint32_t) data[i * 4 + 0] << 24)
@@ -34,8 +35,7 @@ static void process_one_block(sha256_context_t *ctx, const uint8_t *data)
              | ((uint32_t) data[i * 4 + 3] <<  0);
     }
     for (i = 16; i <= 63; ++i) {
-        m[i] = Q1(m[i -  2]) + m[i -  7] +
-               Q0(m[i - 15]) + m[i - 16] ;
+        m[i] = Q1(m[i -  2]) + m[i -  7] + Q0(m[i - 15]) + m[i - 16];
     }
 
     a = ctx->runninghash[0];
@@ -72,22 +72,17 @@ static void process_one_block(sha256_context_t *ctx, const uint8_t *data)
 
 static void increase_by(uint32_t *biginteger, uint32_t incr)
 {
-    //          high bits           low bits
+    //          high bits            low bits
     //      +---------------+    +---------------+
     //      | biginteger[0] |    | biginteger[1] |
     //      +---------------+    +---------------+
     //
-    uint32_t space;
-
-    space = 0xffffffff - biginteger[1];
-    incr &= 0xffffffff;
+    uint32_t space = 0xffffffff - biginteger[1];
 
     biginteger[1] += incr;
 
     if (incr >= space) {
-        biginteger[1] &= 0xffffffff;
         biginteger[0] += 1;
-        biginteger[0] &= 0xffffffff;
     }
 }
 
@@ -134,20 +129,20 @@ void sha256_update(sha256_context_t *ctx, size_t ilen, const uint8_t *ibuf)
 void sha256_output(sha256_context_t *ctx, uint8_t *obuf)
 {
     sha256_context_t ctx_;
-    uint8_t i;
+    int i;
 
     if (ctx == NULL || obuf == NULL) {
         return;
     }
 
-    // Create another working context, not polluting the original one
+    // Let us use another copy of the context, not polluting the original one
     ctx_ = *ctx;
 
     // Append one 0x80 byte
     i = ctx_.msgchunklen;
     ctx_.msgchunk[i++] = 0x80;
 
-    // Append some number of 0x00 bytes
+    // Append some 0x00 bytes... until the last eight bytes
     if (i >= 57) {
         while (i < 64) {
             ctx_.msgchunk[i++] = 0x00;
@@ -161,21 +156,21 @@ void sha256_output(sha256_context_t *ctx, uint8_t *obuf)
 
     // Append eight bytes representing the message bit length (big-endian)
     increase_by(ctx_.totalbitlen, ctx_.msgchunklen * 8);
-    ctx_.msgchunk[56] = (ctx_.totalbitlen[0] >> 24) & 0xff;
-    ctx_.msgchunk[57] = (ctx_.totalbitlen[0] >> 16) & 0xff;
-    ctx_.msgchunk[58] = (ctx_.totalbitlen[0] >>  8) & 0xff;
-    ctx_.msgchunk[59] = (ctx_.totalbitlen[0] >>  0) & 0xff;
-    ctx_.msgchunk[60] = (ctx_.totalbitlen[1] >> 24) & 0xff;
-    ctx_.msgchunk[61] = (ctx_.totalbitlen[1] >> 16) & 0xff;
-    ctx_.msgchunk[62] = (ctx_.totalbitlen[1] >>  8) & 0xff;
-    ctx_.msgchunk[63] = (ctx_.totalbitlen[1] >>  0) & 0xff;
+    ctx_.msgchunk[56] = (uint8_t) (ctx_.totalbitlen[0] >> 24);
+    ctx_.msgchunk[57] = (uint8_t) (ctx_.totalbitlen[0] >> 16);
+    ctx_.msgchunk[58] = (uint8_t) (ctx_.totalbitlen[0] >>  8);
+    ctx_.msgchunk[59] = (uint8_t) (ctx_.totalbitlen[0] >>  0);
+    ctx_.msgchunk[60] = (uint8_t) (ctx_.totalbitlen[1] >> 24);
+    ctx_.msgchunk[61] = (uint8_t) (ctx_.totalbitlen[1] >> 16);
+    ctx_.msgchunk[62] = (uint8_t) (ctx_.totalbitlen[1] >>  8);
+    ctx_.msgchunk[63] = (uint8_t) (ctx_.totalbitlen[1] >>  0);
     process_one_block(&ctx_, ctx_.msgchunk);
 
     // Dump the resulting 32-byte hash value
     for (i = 0; i <= 7; ++i) {
-        obuf[i * 4 + 0] = (ctx_.runninghash[i] >> 24) & 0xff;
-        obuf[i * 4 + 1] = (ctx_.runninghash[i] >> 16) & 0xff;
-        obuf[i * 4 + 2] = (ctx_.runninghash[i] >>  8) & 0xff;
-        obuf[i * 4 + 3] = (ctx_.runninghash[i] >>  0) & 0xff;
+        obuf[i * 4 + 0] = (uint8_t) (ctx_.runninghash[i] >> 24);
+        obuf[i * 4 + 1] = (uint8_t) (ctx_.runninghash[i] >> 16);
+        obuf[i * 4 + 2] = (uint8_t) (ctx_.runninghash[i] >>  8);
+        obuf[i * 4 + 3] = (uint8_t) (ctx_.runninghash[i] >>  0);
     }
 }
